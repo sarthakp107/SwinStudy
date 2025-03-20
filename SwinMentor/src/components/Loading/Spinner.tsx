6bfa826b9f602134e09a08cd29296ad4b83a926a
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-03-20 14:28:52 +1100

    implemented spinner component

--- SwinMentor/src/components/Loading/Spinner.tsx
+++ SwinMentor/src/components/Loading/Spinner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Spinner: React.FC = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+      <div className="w-8 h-8 border-4 border-t-transparent border-red-500 rounded-full animate-spin"></div>
     </div>
   );
 };
