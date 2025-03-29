2864e7e46533cbd37b8349a980ae9dfd11adf32c
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-03-29 18:42:21 +1100

    navigate to dashboard after survey completion

--- SwinMentor/src/Hooks/Database/useHandleSurveySubmit.tsx
+++ SwinMentor/src/Hooks/Database/useHandleSurveySubmit.tsx
@@ -5,6 +5,7 @@ import { useUpdateUnitsInProfile } from "@/Hooks/Database/update/useUpdateUnitsI
 import { useAuthContext } from "@/Hooks/Context/useAuthContext";
 import { NUMBER_OF_CURRENT_UNITS } from "@/config/Constants";
 import { useUpdateSurveyStatus } from "./update/useUpdateSurveyStatus";
+import { useNavigate } from "react-router-dom";
 
 export const useHandleSurveySubmit = () => {
     const { state } = useSurveyContext();
@@ -12,6 +13,7 @@ export const useHandleSurveySubmit = () => {
     const { updateDegree, error: updateDegreeError } = useUpdateDegreeInProfile();
     const { user } = useAuthContext();
     const { updateSurveyStatus, error: statusUpdateError } = useUpdateSurveyStatus();
+    const navigate = useNavigate();
 
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
@@ -45,6 +47,7 @@ export const useHandleSurveySubmit = () => {
             if (updateUnitsErrror) {
                 <div className='error'>An Error Occured while updating Degree: {updateUnitsErrror}</div>
             }
+            navigate('/dashboard');
 
         } catch (err) {
             setError("An error occurred while submitting the survey. Please try again.");
