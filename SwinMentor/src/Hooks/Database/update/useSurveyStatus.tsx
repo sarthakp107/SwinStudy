a1bf474242235e82bf672e1ad03058f4e196823d
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-03-30 16:17:43 +1100

    Removed update function in useSurveyStatus

--- SwinMentor/src/Hooks/Database/update/useSurveyStatus.tsx
+++ SwinMentor/src/Hooks/Database/update/useSurveyStatus.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 export const useSurveyStatus = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    const [isSuccess, setIsSuccess] = useState(false);
 
     const [hasSubmittedSurvey, setHasSubmittedSurvey] = useState<boolean>(false);
     const { user } = useAuthContext();
@@ -39,43 +38,5 @@ export const useSurveyStatus = () => {
         fetchSurveyStatus();
     }, [user?.id])
 
-
-    const updateSurveyStatus = async (): Promise<boolean> => {
-        setIsLoading(true);
-        setError(null);
-        setIsSuccess(false);
-
-        if (!user?.id) {
-            setError("No user ID");
-        }
-
-        try {
-            const { error } = await supabase
-                .from("profile")
-                .update({
-                    "hasSubmittedSurvey": true
-                })
-                .eq("id", user?.id);
-
-            if (error) {
-                setError(error.message);
-                return false;
-            }
-
-            setIsSuccess(true);
-            return true;
-
-        } catch (err: unknown) {
-            if (err instanceof Error) {
-                setError(err.message);
-            } else {
-                setError("An unknown error occurred");
-            }
-            return false;
-        } finally {
-            setIsLoading(false);
-        }
-
-    }
-    return { hasSubmittedSurvey, updateSurveyStatus, isLoading, error, isSuccess }
+    return { hasSubmittedSurvey, isLoading, error }
 }
