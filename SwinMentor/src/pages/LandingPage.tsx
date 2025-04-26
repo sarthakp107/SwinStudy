bcb3e7642d0b67f5d7973e9681a7db271a8c6105
prod-chirag <chiragluitel32@gmail.com>
2025-04-26 10:26:21 +1000

    Splash Redesigned Proposed

--- SwinMentor/src/pages/LandingPage.tsx
+++ SwinMentor/src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
 import Spinner from '@/components/Loading/Spinner';
 import useUserProfile from '@/Hooks/GetUserInfo/useUserProfile';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LandingHero from '@/components/LandingPage/LandingHero';
 import FeatureBlock from '@/components/LandingPage/FeatureBlock';
 import ProblemBlock from '@/components/LandingPage/ProblemBlock';
@@ -8,21 +8,39 @@ import BuiltForSwinBlock from '@/components/LandingPage/BuiltForSwinBlock';
 import CTABlock from '@/components/LandingPage/CTABlock';
 import SupaDemoFlashcards from '@/components/LandingPage/SupaDemoFlashcards';
 import { useAuthContext } from '@/Hooks/Context/useAuthContext';
+import SplashScreen from '@/components/SplashScreen';
 
 const LandingPage: React.FC = () => {
   const {user} = useAuthContext();
   const {loading } = useUserProfile(); 
+  const [splashDone, setSplashDone] = useState(false);
+  const [hasSeenSplash] = useState(() => {
+    return localStorage.getItem('hasSeenSplash') === 'true';
+  });
+
+  useEffect(() => {
+    if (!hasSeenSplash) {
+      const timer = setTimeout(() => {
+        setSplashDone(true);
+        localStorage.setItem('hasSeenSplash', 'true'); // mark as seen
+      }, 4000); // duration of splash
+
+      return () => clearTimeout(timer);
+    } else {
+      setSplashDone(true); // skip splash
+    }
+  }, [hasSeenSplash]);
 
   if (loading) return <Spinner />; 
   return (
     <div className="overflow-hidden"> 
+    {!hasSeenSplash && !splashDone && (<div className="fixed inset-0 z-50 bg-white flex items-center justify-center"> <SplashScreen /> </div>)}
       <LandingHero />
       {!user && <ProblemBlock />}
       {!user && <SupaDemoFlashcards />}
       <FeatureBlock />
       <BuiltForSwinBlock />
       <CTABlock />
-       {/* <Footer /> */}
     </div>
   );
 };
