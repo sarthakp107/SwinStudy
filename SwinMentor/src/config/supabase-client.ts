5dc838db01d4abb23bd0c1aad3288e274ee6cacc
Chirag Luitel <chiragluitel32@gmail.com>
2025-06-23 16:41:00 +1000

    Delete SwinMentor directory

--- SwinMentor/src/config/supabase-client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://abqfvrrqqaihqrcjxdzv.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANNON_KEY as string
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase
