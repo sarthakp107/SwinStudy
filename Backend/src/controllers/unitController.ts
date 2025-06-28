77ea601a208220a987ed229ea3aa07a465d84f4d
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-06-28 23:48:09 +1000

    checking db test

--- Backend/src/controllers/unitController.ts
+++ Backend/src/controllers/unitController.ts
@@ -4,13 +4,13 @@ import { Request, Response } from "express";
 
 export const getAllUnits = async(req: Request, res : Response) => {
 
-    // try{
-    //    const result = await query('SELECT * FROM all_units');
-    //     res.json(result.rows);
-    // }catch(error){
-    //     console.log("Error: ", error);
-    // }
-   await res.send("controller is good");
+    try{
+       const result = await query('SELECT * FROM all_units');
+        res.json(result.rows);
+    }catch(error){
+        console.log("Error: ", error);
+    }
+//    await res.send("controller is good");
 };
 
 export const getHome = async(req: Request, res : Response) => {
