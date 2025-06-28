0ae9f7e5e85182c36b2aa8bf14a1c1787509dd9d
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-06-29 00:13:29 +1000

    im dubm af changes the table name to correct one

--- Backend/src/models/messageModels.ts
+++ Backend/src/models/messageModels.ts
@@ -2,7 +2,7 @@ import { query } from "../database";
 
 export const saveMessages = async (unitName: string, sender: string, message: string) => {
   const sql = `
-    INSERT INTO messages (unit_name, sender, message, sent_at)
+    INSERT INTO unit_messages (unit_name, sender, message, sent_at)
     VALUES ($1, $2, $3, NOW())
   `;
   await query(sql, [unitName, sender, message]);
