9590ae6ccfe8258e3e4f752d63409187595a5bfe
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-07-03 01:43:25 +1000

    last hope

--- Backend/src/sockets/indivChats.ts
+++ Backend/src/sockets/indivChats.ts
@@ -2,29 +2,33 @@ import { Socket, Server } from "socket.io";
 
 export const handleIndivChat = (io: Server) => {
     io.on("connection", (socket: Socket) => {
+
+        console.log("indiv Socket server connected:", socket.id);
+
+        socket.on("disconnect", () => {
+            console.log(`Socket disconnected: ${socket.id}`);
+        })
+
         socket.on("join_private_room", (roomName: string) => {
             socket.join(roomName);
+
             console.log(`private: ${socket.id} joined room ${roomName}`);
         })
 
         socket.on("private_message", async ({ roomName, sender, receiver, message }) => {
             console.log("Received private_message event", { roomName, sender, receiver, message });
             try {
-                io.to(roomName).emit("private_message", { sender, message });
+                io.to(roomName).emit("private_message", { sender, receiver, message });
                 const res = await fetch("https://swinstudy.com/api/chat/postIndividualMessage", {
                     method: "POST",
                     headers: {
                         "content-type": "application/json",
                     },
-                    body: JSON.stringify({ sender_id: sender,receiver_id: receiver, message: message })
+                    body: JSON.stringify({ sender_id: sender, receiver_id: receiver, message: message })
                 })
             } catch (err) {
                 console.log(err);
             }
-        })
-
-        socket.on("disconnect", () => {
-            console.log(`Socket disconnected: ${socket.id}`);
         })
 
     });
