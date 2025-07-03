9ad11d544c534556d938742a287ccc04dfa64098
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-07-03 12:18:53 +1000

    debugging 5- chirag

--- Backend/src/sockets/indivChats.ts
+++ Backend/src/sockets/indivChats.ts
@@ -18,14 +18,15 @@ export const handleIndivChat = (io: Server) => {
         socket.on("private_message", async ({ roomName, sender, receiver, message }) => {
             console.log("Received private_message event", { roomName, sender, receiver, message });
             try {
-                io.to(roomName).emit("private_message", { sender, receiver, message });
+                
                 const res = await fetch("https://swinstudy.com/api/chat/postIndividualMessage", {
                     method: "POST",
                     headers: {
                         "content-type": "application/json",
                     },
-                    body: JSON.stringify({ sender_id: sender, receiver_id: receiver, message: message })
+                    body: JSON.stringify({ sender_id: "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11",  receiver_id: "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11", message: "try chirag" })
                 })
+                io.to(roomName).emit("private_message", { sender, receiver, message });
                 console.log(res);
             } catch (err) {
                 console.log(err);
