52106adb854b987ef28acb23bdd1c2a74bb9a143
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-07-14 15:28:52 +1000

    test

--- Backend/src/controllers/userController.ts
+++ Backend/src/controllers/userController.ts
@@ -13,3 +13,12 @@ export const createHash = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Server error" });
   }
 }
+export const test = async (req: Request, res: Response) => {
+ 
+  try {
+    console.log("working test userRoutes")
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+}
--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { createHash } from '../controllers/userController';
+import { createHash, test } from '../controllers/userController';
 
 const userRouter = express.Router();
 
 userRouter.post("/create-public-id", createHash)
+userRouter.get("/test", test);
+
 
 export default userRouter;
